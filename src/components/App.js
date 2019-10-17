2946a657ea52c64c315be6ca23116c9e574ebe7f
saikumargade <gade.saikumar1@gmail.com>
2019-10-17 16:49:35 +0530

    added navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../styles/App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      {/* <h2>Hello World!</h2> */}
+      {/* navbar
+          categories
+          search
+          sign in
+          cart */}
+        <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Navbar.css'
+export default class Navbar extends React.Component{
+    render(){
+        return(
+            <div className='navbar'>
+                <h3>hello</h3>
+            </div>
+            
+        )
+    }
+}
