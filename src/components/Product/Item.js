b7538cba11ddef4371da7382672ebec56ba94113
saikumargade <gade.saikumar1@gmail.com>
2019-10-24 12:24:03 +0530

    able to store props recieved from link tag and retrieve them even on reloading page

--- src/components/Product/Item.js
+++ src/components/Product/Item.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Item.css'
 export default class Item extends React.Component{
+    componentWillMount(){
+        if(!sessionStorage.getItem('img'))
+        sessionStorage.setItem('img',this.props.location.image.display);
+    }
     render(){
-
         return(
             <div className='item'>
-                <img src={this.props.location.image.display}></img>
-
+                <img src={(!(sessionStorage.getItem('img')))?this.props.location.image.display:sessionStorage.getItem('img')}></img>
             </div>
         )
     }
