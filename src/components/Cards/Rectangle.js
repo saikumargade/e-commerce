8f76f510ca7651eab7b21eedb388155cbcdf78c5
saikumargade <gade.saikumar1@gmail.com>
2019-10-24 23:17:44 +0530

    routing to item page with the data(img) sent. -(issue in the previous commit)

--- src/components/Cards/Rectangle.js
+++ src/components/Cards/Rectangle.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom';
 export default class Rectangle extends React.Component{
     render(){
         // console.log("Image: ", this.props.img)
-        let image=this.props.img;
+        console.log("id:",this.props.unique)
         return(
             <div className='box'>
                 <h3>{this.props.heading}</h3>
                 <Link to={{pathname:'/item',image:{display:this.props.img}}}><img src={this.props.img} alt='imgdescription'></img></Link>
-                <h5>{this.props.description}</h5>
+                <p>{this.props.description}</p>
             </div>
         )
     }
--- src/components/Product/Item.js
+++ src/components/Product/Item.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Item.css'
 export default class Item extends React.Component{
-    componentWillMount(){
-        if(!sessionStorage.getItem('img'))
-        sessionStorage.setItem('img',this.props.location.image.display);
+    static getDerivedStateFromProps(props,state){
+        if(props.location.image||!sessionStorage.getItem('img'))
+        sessionStorage.setItem('img',props.location.image.display);
     }
     render(){
         return(
